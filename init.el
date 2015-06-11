e288ed540032995e415fccdb62a1cd9cd341fe7f
wanghao <wanghao1891@126.com>
2015-06-11 08:30:47 +0000

    remove the comment of custom-file in init.el.

--- init.el
+++ init.el
@@ -123,9 +123,9 @@
 ;;----------------------------------------------------------------------------
 ;; Variables configured via the interactive 'customize' interface
 ;;----------------------------------------------------------------------------
-;(setq custom-file (expand-file-name "custom.el" user-emacs-directory))
-;(when (file-exists-p custom-file)
-;  (load custom-file))
+(setq custom-file (expand-file-name "custom.el" user-emacs-directory))
+(when (file-exists-p custom-file)
+  (load custom-file))
 
 
 ;;----------------------------------------------------------------------------
