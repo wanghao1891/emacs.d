2e5a220bf4c04747c99e10eb8d5a95f68e3ccce1
wanghao <wanghao1891@126.com>
2015-07-08 21:24:47 +0800

    Add view-tag-other-window

--- lisp/init-ctags.el
+++ lisp/init-ctags.el
@@ -10,4 +10,13 @@
   (shell-command
    (format "ctags -f %s%s -e -R %s" path-to-ctags tag-name (directory-file-name dir-name))))
 
+;;; View tags other window
+(defun view-tag-other-window (tagname &optional next-p regexp-p)
+  "Same as `find-tag-other-window' but doesn't move the point"
+  (interactive (find-tag-interactive "View tag other window: "))
+  (let ((window (get-buffer-window)))
+    (find-tag-other-window tagname next-p regexp-p)
+    (recenter 0)
+    (select-window window)))
+
 (provide 'init-ctags)
