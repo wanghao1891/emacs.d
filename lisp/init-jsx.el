79458a9d6d25ef540e8137c41d44d077554ae2b1
wanghao1891 <wanghao1891@126.com>
2016-03-13 13:31:47 +0800

    Add flycheck with eslint for jsx.

--- lisp/init-jsx.el
+++ lisp/init-jsx.el
@@ -1,3 +1,7 @@
+;;; package --- Summary
+;;; Commentary:
+;;; Code:
+
 (require-package 'web-mode)
 (add-to-list 'auto-mode-alist '("\\.jsx\\'" . web-mode))
 
@@ -9,4 +13,28 @@
   )
 (add-hook 'web-mode-hook  'my-web-mode-hook)
 
+;; http://www.flycheck.org/manual/latest/index.html
+(require 'flycheck)
+
+;; turn on flychecking globally
+(add-hook 'after-init-hook #'global-flycheck-mode)
+
+;; disable jshint since we prefer eslint checking
+(setq-default flycheck-disabled-checkers
+              (append flycheck-disabled-checkers
+                      '(javascript-jshint)))
+
+;; use eslint with web-mode for jsx files
+(flycheck-add-mode 'javascript-eslint 'web-mode)
+
+;; customize flycheck temp file prefix
+;;(setq-default flycheck-temp-prefix ".flycheck")
+
+;; disable json-jsonlist checking for json files
+(setq-default flycheck-disabled-checkers
+              (append flycheck-disabled-checkers
+                          '(json-jsonlist)))
+
 (provide 'init-jsx)
+
+;;; init-jsx.el ends here
