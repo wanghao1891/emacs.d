2e38312fda9f734d56f06532685e6ca41976abec
wanghao1891 <wanghao1891@126.com>
2016-10-26 10:28:40 +0800

    synchronize elpa to github.

---
+++ elpa/s-20140910.334/s-autoloads.el
@@ -0,0 +1,15 @@
+;;; s-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil nil ("s.el") (21872 29938 645586 924000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; s-autoloads.el ends here
